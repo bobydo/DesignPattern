ae3e9d81574e5038b455c6996453a77f7f7ea2aa
Shenyi Bao <baoshenyi@gmail.com>
2023-04-23 15:51:17 -0600

    Factory

---
+++ Factory/FactoryClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    using System;
+
+    // Define a common interface for all products
+    interface IProduct
+    {
+        void DoSomething();
+    }
+
+    // Define concrete implementations of the product interface
+    public class ConcreteProductA : IProduct
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Doing something in ConcreteProductA");
+        }
+    }
+
+    public class ConcreteProductB : IProduct
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Doing something in ConcreteProductB");
+        }
+    }
+
+    // Define the factory class
+    class Factory<T> where T : IProduct, new()
+    {
+        // The factory method that creates and returns an instance of the concrete product
+        public T CreateProduct()
+        {
+            return new T();
+        }
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use the factory to create instances of the products
+            var factoryA = new Factory<ConcreteProductA>();
+            var productA = factoryA.CreateProduct();
+            productA.DoSomething();
+
+            var factoryB = new Factory<ConcreteProductB>();
+            var productB = factoryB.CreateProduct();
+            productB.DoSomething();
+
+            Console.ReadLine();
+        }
+    }
+}
